4d19db51154a5c5918405a30f7fd3245f1ffcad8
dheerutocool <dheerutocool@gmail.com>
2016-10-23 03:15:36 -0400

    first commit

---
+++ basicexample/src/basicexample123/baggi.java
@@ -0,0 +1,17 @@
+package basicexample123;
+
+public class baggi 
+{
+	
+	// variables(datatypes )/methods
+
+int a = 1;
+int b = 2;
+
+
+//public void add(){
+//	int c = a+b;
+//	System.out.println("addition of two number" + c);
+	
+	
+}
---
+++ basicexample/src/basicexample123/myclass.java
@@ -0,0 +1,9 @@
+package basicexample123;
+
+public class myclass {
+
+	public static void main(String[] args) {
+System.out.println("hello");
+	}
+
+}
